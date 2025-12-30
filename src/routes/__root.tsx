afedcd628966c401a511e2f7bc3690de737936b9
Wilton Baltazar <wiltonbaltazar@gmail.com>
2025-12-30 14:57:47 +0200

    Inital commit

---
+++ src/routes/__root.tsx
@@ -0,0 +1,35 @@
+import { HeadContent, Outlet, createRootRoute } from '@tanstack/react-router'
+import { TanStackRouterDevtoolsPanel } from '@tanstack/react-router-devtools'
+import { TanStackDevtools } from '@tanstack/react-devtools'
+
+export const Route = createRootRoute({
+  head: () => ({
+    meta: [
+      {
+        name: 'description',
+        content: 
+        'Share, explore and build on the best startup ideas and side hustles'
+      },
+      {
+        title: 'Idea Drop - Your Idea Hub',
+      },
+    ]
+  }),
+  component: () => (
+    <>
+      <HeadContent />
+      <Outlet />
+      <TanStackDevtools
+        config={{
+          position: 'bottom-right',
+        }}
+        plugins={[
+          {
+            name: 'Tanstack Router',
+            render: <TanStackRouterDevtoolsPanel />,
+          },
+        ]}
+      />
+    </>
+  ),
+})
